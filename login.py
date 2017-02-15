6f2c3bf8396744969720571ab3a81b680f1ae3de
yangchao <yangchao@douban.com>
2017-02-15 10:53:47 +0800

    update profile link tag class

--- login.py
+++ login.py
@@ -75,7 +75,7 @@ class LoginBot(object):
         html = BeautifulSoup(login_response.content)
 
         try:
-            profile_link = html.find('a', {'class': 'profile-me'})['href']
+            profile_link = html.find('a', {'class': 'my-profile js-gps-track'})['href']
         except TypeError:
             problem = self._parse_error_message(html.text)
             raise LoginError('Failed to login: {}'.format(problem))
